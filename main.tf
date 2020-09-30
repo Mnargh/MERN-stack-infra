fe90c42d994fc93faac379f2e290f05bbb113dbd
Tom Brandon <tom.brandon@rbs.co.uk>
2020-09-30 15:13:20 +0100

    Added dynamodb state locking mechanism

--- main.tf
+++ main.tf
@@ -208,3 +208,21 @@ resource "aws_s3_bucket" "terraform-state-storage-s3" {
       Name = "S3 Remote Terraform State Store"
     }      
 }
+
+
+# Dynamodb table for locking the state file
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "terraform-state-lock-dynamo"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+ 
+  tags {
+    Name = "DynamoDB Terraform State Lock Table"
+  }
+}
--- terraform.tf
+++ terraform.tf
@@ -2,6 +2,7 @@ terraform {
  backend “s3” {
  encrypt = true
  bucket = "terraform-remote-state-storage-s3"
+ dynamodb_table = "terraform-stack-lock-dynamo"
  region = eu-west-1
  key = ./main.tf
  }
