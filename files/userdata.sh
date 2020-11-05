1bb1cb06de08ca3a8b63ccafbacd9bf629d61eff
Tom Brandon <tom.brandon@rbs.co.uk>
2020-11-05 02:22:02 +0000

    Passing remaining env vars thru tf command to run docker container

--- files/userdata.sh
+++ files/userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # add extra ssh keys for convenience
-sudo cat <<EOF >>/home/ec2-user/.ssh/authorized_keys
+cat <<EOF >>/home/ec2-user/.ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC+sMk/PqrzoQkfpRkdyResNwlzTFRZ0rBRWJlUdNZHP1yfsbWU/6C+qjaZqQ2VmM9Rv6nhXpfOpVZkIVvphS9knA5yR0w6zTy1v5E90tBUCCyjHRAjlQY2kdcwWg9zNSI6NvuKGM8281oKV8gP+gdXPgZjjdhG9U8ael3ftRs3508MJghIeGCEYq6r2llomE2Jnb8UOKn4ybt5mr6D24Xk1HlST/M/DrauSBGseMxrcoc66f/VkTNPKj2bniE8zWzgjB1ZxpMZyruovfDGOYOyl15bSodpyEXsLRFCsLA3i+m+cLLnTSTdQK7nROfYb/2tVXwa1YwUhgI/4DrmNjfOtpgTUChknWZt1Nqg/eC3y8fdYTaXdHUOgfmiGtA7bw9uu7ZTsQ2zpSZXgkPTLItPN4irOEXaeditBqUb6AoCqVhdMq/pJ+ot4L6WpYEJw67vAAJrSsZLh5R/X6GSFMHoa9t7xL2dbPNfo2GZ8fs3O/wmd86PpcwDamLJwLsS0ztYKLlW8gsZLnS0aRuCyTywSSKw7jopSjYPTxIS1M6rnrI72VYIL5rrQy3F22jYVkA30tqmyzOL/gVIVQjvafn9AGr8VnQ062d0Ud3HkxgaQ9XaT8kmrVDxexZ+pMVmyZGEqG2TlbYnAqB/kqqHenr5r4gLNaDnGbfwajZFdCCbxw== tom.j.v.brandon@gmail.com
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCfdZxWI6H+caoSMHdfYzx/gf12uWIw0YgNGDaO8Uai+rIulY1rTZdjZZZrQGE7U5ImI9CzGn0sAZ9MQo5wlQA7r+95rb962iqmYmKONPhPD+sB2+Z/xcOd9JqIfTYAfgJ7SlNc8P0neX75lI/YKvjPyAEa6qMznNDunGAE8MUrOxdpzgUlXeTEKaY5hI9NIwMuq1AkMlXujf1VwxreYbM6e1DmB2A/T2vzY3uRecRMKDN3STPaTuwnPuf0PLCPR/BYCcJtJOVqLPyZe9AT/lgvEzIN29xrsBCRzclkUjfGGT2zHGwUtlbm1cqDQMRmaY5W7uD9XA/96cMOH4reBNUP stang@ecsd-mbp16
 EOF
@@ -17,11 +17,11 @@ docker pull 674726326575.dkr.ecr.eu-west-1.amazonaws.com/mern-stack:v5
 PUBLIC_IP=$(curl -s ifconfig.io)
 
 curl \
---user "${MONGODB_PUB_API_KEY}:${MONGODB_SECRET_API_KEY}" --digest --include \
+--user "${MONGODB_PUBLIC_API_KEY}:${MONGODB_SECRET_API_KEY}" --digest --include \
 --header "Content-Type: application/json" \
 --request POST "https://cloud.mongodb.com/api/atlas/v1.0/groups/${MONGODB_GROUP_ID}/whitelist?pretty=true" \
 --data "[{
-\"cidrBlock\" : \"${PUBLIC_IP}/32\",
+\"cidrBlock\" : \"$PUBLIC_IP/32\",
 \"comment\" : \"AWS EC2 Instance that host the 'mern-stack'\"
 }]"
 
@@ -47,10 +47,10 @@ cat <<'EOF' >> /home/ec2-user/set-up-container.sh
 #!/bin/bash
 
 docker container run -itd --rm --env DANGEROUSLY_DISABLE_HOST_CHECK=true \
---env MONGODB_ACCESS=$MONGODB_ACCESS \
---env JWT_SECRET=$JWT_SECRET \
---env GITHUB_CLIENT_ID=$GITHUB_CLIENT_ID \
---env GITHUB_SECRET=$GITHUB_SECRET \
+--env "MONGODB_ACCESS=${MONGODB_ACCESS}" \
+--env JWT_SECRET=${JWT_SECRET} \
+--env GITHUB_CLIENT_ID=${GITHUB_CLIENT_ID} \
+--env GITHUB_SECRET=${GITHUB_SECRET} \
 -p 80:3000 -p 5000:5000 --name super-mern-stack 674726326575.dkr.ecr.eu-west-1.amazonaws.com/mern-stack:v5
 EOF
 
--- resources/variables.tf
+++ resources/variables.tf
@@ -8,4 +8,33 @@ variable "env_prefix" {
 
 variable "MONGODB_ACCESS" {
   description = "Establishes connection to access to mongodb atlas"
+  default     = ""
+}
+
+variable "MONGODB_GROUP_ID" {
+  description = "MongoDB Group ID required for whitelisting AWS instance to mongodb atlas"
+  default     = ""
+}
+
+variable "MONGODB_PUBLIC_API_KEY" {
+  description = "MongoDB public api key required for whitelisting AWS instance to mongodb atlas"
+  default     = ""
+}
+
+variable "MONGODB_SECRET_API_KEY" {
+  description = "MongoDB secret api key required for whitelisting AWS instance to mongodb atlas"
+  default     = ""
+}
+
+variable "JWT_SECRET" {
+  description = "Json web token secret to be passed to application container"
+  default     = ""
+}
+variable "GITHUB_CLIENT_ID" {
+  description = "Github client id to be passed to application container"
+  default     = ""
+}
+variable "GITHUB_SECRET" {
+  description = "Github secret to be passed to application container"
+  default     = ""
 }
